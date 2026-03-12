84bb789fbe5eb52cacbf22221dfc1fba5f42d452
Horus Turboss <81076765+docteur-turboss@users.noreply.github.com>
2026-03-12 18:39:03 +0100

    🌐 Update the responses for integrate the response helper

--- src/event/interactionCreate.ts
+++ src/event/interactionCreate.ts
@@ -1,5 +1,6 @@
 import { commands } from "commands";
-import { ChatInputCommandInteraction, Collection, Events, MessageFlags } from "discord.js";
+import { ChatInputCommandInteraction, Events, MessageFlags } from "discord.js";
+import { followUpError, replyError } from "utils/discord/reply";
 import { logger } from "utils/logger/logger";
 
 export const data = {
@@ -13,17 +14,24 @@ export const main = async (interaction: ChatInputCommandInteraction) => {
 
 	if (!command) {
 		logger.error(`No command matching ${interaction.commandName} was found.`);
-		return;
+		return await replyError(interaction, { 
+			key: "errors.no_command_found", 
+			ephemeral: true
+		});
 	}
 
 	if (timestamps.has(interaction.user.id)) {	
 		const expirationTime = timestamps.get(interaction.user.id)??0 + cooldownAmount;
 		if (Date.now() < expirationTime) {
 			const expiredTimestamp = Math.round(expirationTime / 1_000);
-			return interaction.reply({
-				content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`,
-				flags: MessageFlags.Ephemeral,
-			});
+			return await replyError(interaction, {
+				key: "cooldown.active",
+				ephemeral: true,
+				vars: {
+					command: command.data.name,
+					time: `<t:${expiredTimestamp}:R>`
+				}
+			})
 		}
 	}
 
@@ -32,14 +40,14 @@ export const main = async (interaction: ChatInputCommandInteraction) => {
 	} catch (error) {
 		logger.error("Error executing command " + interaction.commandName, error as Record<string, unknown>);
 
-		if (interaction.replied || interaction.deferred) return await interaction.followUp({
-				content: 'There was an error while executing this command!',
-				flags: MessageFlags.Ephemeral,
-			});
-    
-    await interaction.reply({
-      content: 'There was an error while executing this command!',
-      flags: MessageFlags.Ephemeral,
-    });
+		if (interaction.replied || interaction.deferred) return await followUpError(interaction, {
+			key: "errors.command_execution",
+			ephemeral: true
+		})
+			
+    await replyError(interaction, {
+			key: "errors.command_execution",
+			ephemeral: true
+		});
   }
 };

a2241a784084c1ea7169e594612f6d2f35ae5f4f
Horus Turboss <81076765+docteur-turboss@users.noreply.github.com>
2026-03-24 14:18:51 +0100

    🚧 Add GatewayIntent on client

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,7 +3,27 @@ import { env } from "config/env";
 import { Client, GatewayIntentBits } from "discord.js";
 
 const client = new Client({ intents: [
-  GatewayIntentBits.Guilds
+  GatewayIntentBits.AutoModerationConfiguration,
+  GatewayIntentBits.AutoModerationExecution,
+  GatewayIntentBits.DirectMessageReactions,
+  GatewayIntentBits.GuildMessageReactions,
+  GatewayIntentBits.GuildScheduledEvents,
+  GatewayIntentBits.DirectMessageTyping,
+  GatewayIntentBits.DirectMessagePolls,
+  GatewayIntentBits.GuildMessageTyping,
+  GatewayIntentBits.GuildIntegrations,
+  GatewayIntentBits.GuildMessagePolls,
+  GatewayIntentBits.GuildExpressions,
+  GatewayIntentBits.GuildVoiceStates,
+  GatewayIntentBits.GuildModeration,
+  GatewayIntentBits.MessageContent,
+  GatewayIntentBits.DirectMessages,
+  GatewayIntentBits.GuildPresences,
+  GatewayIntentBits.GuildMessages,
+  GatewayIntentBits.GuildWebhooks,
+  GatewayIntentBits.GuildInvites,
+  GatewayIntentBits.GuildMembers,
+  GatewayIntentBits.Guilds,
 ]});
 
 event.loadEvents(client);
